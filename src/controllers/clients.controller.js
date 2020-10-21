2cf0284ba40f72fe6ddc7b0c0502cc55c2ffba5d
JairKevinFG <jkfloresg@uni.pe>
2020-10-21 18:06:45 -0500

    listado clientes!

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -7,10 +7,12 @@ clientsController.createClientForm = async (req,res) => {
     const {name,email,phone,description}  = req.body
     const newClient = new Client({name,email,phone,description})
     await newClient.save()
+    res.redirect('/clients')
 }
 clientsController.renderClients = async (req,res) => {
     const clients = await Client.find()
     res.render('clients/clients',{clients})
+    
 }
 
 clientsController.renderEditForm = (req,res) => {
