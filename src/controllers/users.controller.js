b5740f37b1afaac2047a623b178a2d61273fa933
JairKevinFG <jkfloresg@uni.pe>
2020-10-28 00:55:28 -0500

    validaciones del lado servidor

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -2,6 +2,24 @@ const usersController = {}
 
 
 usersController.signup = (req,res)=> {
+    const errors = []
+    const {name,email,password,confirm_password} = req.body
+    if(password != confirm_password){
+        errors.push({text:'Las contraseñas no coinciden'})
+    }
+    if(password.length < 6){
+        errors.push({text:"Las contraseñas deben ser mayores a 6 caracteres"})
+    }
+    if(errors.length > 0){
+        res.render('index',{errors,
+        name,
+        email,
+        confirm_password,
+        password
+        })
+    }else{
+        res.send("Registrado correctamente")
+    }
     console.log(req.body)
 }
 
